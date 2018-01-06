97cc65ff1e1c00a91e047fc8829fbf6f41f821c5
Tetsuya Taguchi <bouzu.ao@gmail.com>
2018-01-06 10:49:49 +0900

    add: Equality for All

--- lib/dollar.rb
+++ lib/dollar.rb
@@ -10,4 +10,8 @@ class Dollar
   def times(multiplier)
     Dollar.new(@amount * multiplier)
   end
+
+  def equals(dollar)
+    @amount == dollar.amount
+  end
 end
--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -1,13 +1,27 @@
 require 'dollar'
 
-RSpec.describe Dollar, "#times" do
-  context "multiplication" do
-    it "times dollar" do
-      five = Dollar.new(5)
-      product = five.times(2)
-      expect(product.amount).to eq 10
-      product = five.times(3)
-      expect(product.amount).to eq 15
+RSpec.describe Dollar do
+  describe "#times" do
+    context "times repeatedly" do
+      it "gets correct amount" do
+        five = Dollar.new(5)
+        product = five.times(2)
+        expect(product.amount).to eq 10
+        product = five.times(3)
+        expect(product.amount).to eq 15
+      end
+    end
+  end
+
+  describe "#equals" do
+    context "check equality" do
+      it "gets equal amount" do
+        expect(true).to eq Dollar.new(5).equals(Dollar.new(5))
+      end
+
+      it "gets not equal amount" do
+        expect(false).to eq Dollar.new(5).equals(Dollar.new(6))
+      end
     end
   end
 end
