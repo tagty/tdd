8283f9408ec46f36a1610288e932b0f167487680
Tetsuya Taguchi <bouzu.ao@gmail.com>
2018-02-21 12:19:03 +0900

    add: The Root of All Evil

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,38 +1,23 @@
-require 'dollar'
-require 'franc'
-
+require 'money'
 RSpec.describe Money do
   describe "#times" do
     context "repeated multiplication" do
       it "gets multiplied dollar" do
         five = Money.dollar(5)
-        # Other object was created
-        # expect(five.times(2)).to eq Money.dollar(10) 
         expect(five.times(2).amount).to eq Money.dollar(10).amount 
-        expect(five.times(3).amount).to eq Money.dollar(15).amount
-      end
-
-      it "gets multiplied franc" do
-        five = Money.franc(5)
-        expect(five.times(2).amount).to eq Money.franc(10).amount 
-        expect(five.times(3).amount).to eq Money.franc(15).amount
         expect(five.times(3).amount).to eq Money.dollar(15).amount
       end
     end
   end
-
   describe "#equals" do
     context "test equality" do
       it "gets [equal/not equal] amount" do
         expect(true).to eq Money.dollar(5).equals(Money.dollar(5))
         expect(false).to eq Money.dollar(5).equals(Money.dollar(6))
-        expect(true).to eq Money.franc(5).equals(Money.franc(5))
-        expect(false).to eq Money.franc(5).equals(Money.franc(6))
         expect(false).to eq Money.franc(5).equals(Money.dollar(6))
       end
     end
   end  
-
   describe "#currency" do
     context "test currency" do
       it "has currency" do
