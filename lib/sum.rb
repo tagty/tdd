7b2cf294755352e4b153d4c92ec6174035c71cc0
Tetsuya Taguchi <bouzu.ao@gmail.com>
2018-04-02 07:58:45 +0900

    add: Abstraction, Finally

--- lib/sum.rb
+++ lib/sum.rb
@@ -1,9 +1,19 @@
 class Sum
   attr_accessor :augend, :addend
+
   def initialize(augend, addend)
     self.augend = augend
     self.addend = addend
   end
+
+  def times(multiplier)
+    Sum.new(augend.times(multiplier), addend.times(multiplier))
+  end
+
+  def plus(addend)
+    Sum.new(self, addend)
+  end
+
   def reduce(bank, to)
     amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount
     Money.new(amount, to)
--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -9,7 +9,19 @@ RSpec.describe Money do
       expect(five.times(2).amount).to eq Money.dollar(10).amount
       expect(five.times(3).amount).to eq Money.dollar(15).amount
     end
+
+    it "times sum" do
+      five_bucks = Money.dollar 5
+      ten_francs = Money.franc 10
+      bank = Bank.new
+      bank.add_rate("CHF", "USD", 2)
+      sum = Sum.new(five_bucks, ten_francs).times 2
+      result = bank.reduce(sum, "USD")
+      expect(result.amount).to eq Money.dollar(20).amount
+      expect(result.currency).to eq Money.dollar(20).currency
+    end
   end
+
   describe "#equals" do
     it "gets [equal/not equal] amount" do
       expect(true).to eq Money.dollar(5).equals(Money.dollar(5))
@@ -17,12 +29,14 @@ RSpec.describe Money do
       expect(false).to eq Money.franc(5).equals(Money.dollar(6))
     end
   end
+
   describe "#currency" do
     it "has currency" do
       expect("USD").to eq Money.dollar(1).currency
       expect("CHF").to eq Money.franc(1).currency
     end
   end
+
   describe "#plus" do
     it "gets simple addition" do
       five = Money.dollar 5
@@ -31,13 +45,26 @@ RSpec.describe Money do
       reduced = bank.reduce(sum, "USD")
       expect(Money.dollar(10).amount).to eq reduced.amount
     end
+
     it "return sum" do
       five = Money.dollar 5
       sum = five.plus five
       expect(sum.augend).to eq five
       expect(sum.addend).to eq five
     end
+
+    it "plus sum of money" do
+      five_bucks = Money.dollar 5
+      ten_francs = Money.franc 10
+      bank = Bank.new
+      bank.add_rate("CHF", "USD", 2)
+      sum = Sum.new(five_bucks, ten_francs).plus(five_bucks)
+      result = bank.reduce(sum, "USD")
+      expect(result.amount).to eq Money.dollar(15).amount
+      expect(result.currency).to eq Money.dollar(15).currency
+    end
   end
+
   describe "#reduce" do
     it "reduce sum" do
       sum = Sum.new(Money.dollar(3), Money.dollar(4))
@@ -45,11 +72,13 @@ RSpec.describe Money do
       result = bank.reduce(sum, "USD")
       expect(result.amount).to eq Money.dollar(7).amount
     end
+
     it "reduce money" do
       bank = Bank.new
       result = bank.reduce(Money.dollar(1), "USD")
       expect(result.amount).to eq Money.dollar(1).amount
     end
+
     it "reduce different currency" do
       bank = Bank.new
       bank.add_rate "CHF", "USD", 2
@@ -57,6 +86,7 @@ RSpec.describe Money do
       expect(result.amount).to eq Money.dollar(1).amount
       expect(result.currency).to eq Money.dollar(1).currency
     end
+
     it "returns mixed addition" do
       five_bucks = Money.dollar 5
       ten_francs = Money.franc 10
@@ -67,6 +97,7 @@ RSpec.describe Money do
       expect(result.currency).to eq Money.dollar(10).currency
     end
   end
+
   describe "#rate" do
     it "returns identity rate" do
       expect(Bank.new.rate("USD", "USD")).to eq 1
