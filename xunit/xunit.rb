45d7b16013eea12e86e874d87b561718e070ccba
Tetsuya Taguchi <bouzu.ao@gmail.com>
2018-04-29 07:39:09 +0900

    add: Set the table

--- xunit/xunit.rb
+++ xunit/xunit.rb
@@ -5,28 +5,37 @@ class TeatCase
   def initialize(name)
     @name = name
   end
+  def set_up
+  end
   def run
     send(@name)
   end
 end
 class WasRun < TeatCase
-  attr_reader :was_run
-  def initialize(name)
+  attr_reader :was_run, :was_set_up
+  def set_up
     @was_run = false
-    super(name)
+    @was_set_up = true
   end
   def test_method
     @was_run = true 
   end
 end
 class TestCaseTest < TeatCase
+  #attr_reader :test
+  def set_up
+    @test = WasRun.new 'test_method'
+  end
   def test_running
-    test = WasRun.new 'test_method'
-    assert_equal(false, test.was_run)
-    test.run
-    assert_equal(true, test.was_run)
+    @test.run
+    assert_equal(true, @test.was_run)
+  end
+  def test_set_up
+    @test.run
+    assert_equal(true, @test.was_set_up)
   end
 end
 
-TestCaseTest.new('test_running').run
+TestCaseTest.new('test_running').set_up.run
+TestCaseTest.new('test_set_up').set_up.run
 
