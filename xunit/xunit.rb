094805e59ee613f0f1fdf2f86aee4fff259d7c80
Tetsuya Taguchi <bouzu.ao@gmail.com>
2018-05-20 06:56:30 +0900

    add: Counting

--- xunit/xunit.rb
+++ xunit/xunit.rb
@@ -1,6 +1,5 @@
 require 'test/unit/assertions'
 include Test::Unit::Assertions
-
 class TeatCase
   def initialize(name)
     @name = name
@@ -10,9 +9,12 @@ class TeatCase
   def tear_down
   end
   def run
+    result = TestResult.new
+    result.test_started
     set_up
     send(@name)
     tear_down
+    result
   end
 end
 class WasRun < TeatCase
@@ -26,6 +28,9 @@ class WasRun < TeatCase
   def tear_down
     @log += 'tear_down '
   end
+  def test_broken_method
+    raise
+  end
 end
 class TestCaseTest < TeatCase
   def test_template_method
@@ -33,7 +38,29 @@ class TestCaseTest < TeatCase
     test.run
     assert_equal('set_up test_method tear_down ', test.log)
   end
+  def test_result
+    test = WasRun.new 'test_method'
+    result = test.run
+    assert_equal('1 run, 0 failed', result.summary)
+  end
+  def test_failed_result
+    test = WasRun.new 'test_broken_method'
+    result = test.run
+    assert_equal('1 run, 1 failed', result.summary)
+  end
 end
-
+class TestResult
+  def initialize
+    @run_count = 0
+  end
+  def test_started
+    @run_count += 1
+  end
+  def summary
+    "%d run, 0 failed" % @run_count
+  end
+end
 TestCaseTest.new('test_template_method').run
+TestCaseTest.new('test_result').run
+#TestCaseTest.new('test_failed_result').run
 
